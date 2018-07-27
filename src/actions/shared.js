92362be463981d85259634d16eae2ad1145dd72c
Celther <mwfister@gmail.com>
2018-07-27 22:32:05 +1000

    Loading initial data action

---
+++ src/actions/shared.js
@@ -0,0 +1,13 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from './users'
+import { receiveTweets } from './tweets'
+
+export default function handleInitialData() {
+  return (dispatch) => {
+    getInitialData()
+    .then(({users, tweets}) => {
+      dispatch(receiveUsers(users))
+      dispatch(receiveTweets(tweets))
+    })
+  }
+}
