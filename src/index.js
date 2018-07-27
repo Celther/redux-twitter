38d2f46ed5e91d6c17fafc4522686ee2caa0653e
Celther <mwfister@gmail.com>
2018-07-28 01:51:50 +1000

    Adding Reducers to the store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 import './index.css'
+
 import App from './components/App'
+import reducers from '../reducers'
+
+const store = createStore(reducers)
 
 ReactDOM.render(
   <App />,
