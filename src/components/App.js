07d2f06a7b99691f51ae586a07cef98e7ea282cb
Celther <mwfister@gmail.com>
2018-07-27 18:41:34 +1000

    Initial App structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Timeline from './Timeline'
 
 class App extends Component {
   render() {
     return (
       <div>
         Start Code
+        <Timeline />
       </div>
     )
   }
