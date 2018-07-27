f09102ba7c25eb034ce2198788d258f4e696d96b
Celther <mwfister@gmail.com>
2018-07-28 01:26:38 +1000

    RECEIVE_TWEETS update
    Good practice to account for existing state, even if current implementation will have an empty object for state.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -3,7 +3,10 @@ import { RECEIVE_TWEETS, ADD_TWEET, TOGGLE_LIKE } from '../actions/tweets'
 export default function tweets(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
-      return action.tweets
+      return {
+        ...state,
+        ...action.tweets
+      }
     case ADD_TWEET: {
       const { id } = action.tweet
       return {
@@ -20,7 +23,7 @@ export default function tweets(state = {}, action) {
           ...tweet,
           likes: tweet.likes.contains(authedUser)
             ? tweet.filter((user) => user !== authedUser)
-            : tweet.concat(authedUser)
+            : tweet.concat([authedUser])
         }
       }
     }
