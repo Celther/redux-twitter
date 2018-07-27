a3dcbc4d13b60265168475dfb5694a1b9dca67bb
Celther <mwfister@gmail.com>
2018-07-28 00:27:41 +1000

    Tweet Reducers
    Block scoping required for variables inside of the Switch statements.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -4,17 +4,26 @@ export default function tweets(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
       return action.tweets
-    case ADD_TWEET:
+    case ADD_TWEET: {
       const { id } = action.tweet
       return {
         ...state,
         [id]: action.tweet,
       }
-    case TOGGLE_LIKE:
+    }
+    case TOGGLE_LIKE: {
+      const { id, likes, authedUser } = action
+      const tweet = state[id]
       return {
         ...state,
-        
+        [id]: {
+          ...tweet,
+          likes: tweet.likes.contains(authedUser)
+            ? tweet.filter((user) => user !== authedUser)
+            : tweet.concat(authedUser)
+        }
       }
+    }
     default:
       return state
   }
