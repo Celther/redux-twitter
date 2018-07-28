a37b8e1d265255df05acf570b22c20571af616ec
Celther <mwfister@gmail.com>
2018-07-28 23:02:17 +1000

    Rendering Tweet component
    Removed debugging code as well.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import Tweet from './Tweet'
+
 class Timeline extends Component {
   render() {
     const tweetIds = Object.keys(this.props.tweets)
-
-    const date = new Date(Date.now())
-    const formattedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-    const formattedTime = date.getHours() > 12
-      ? `${date.getHours()%12}:${date.getMinutes()}PM`
-      : `${date.getHours()}:${date.getMinutes()}AM`
 
     return (
       <div className="timeline-container">
         <h1>Timeline</h1>
         <ul>
-          <p>{formattedDate}</p>
-          <p>{formattedTime}</p>
-          {/* {tweetIds.map()} */}
+          {tweetIds.map((id) => (
+            <Tweet id={id} key={id}/>
+          ))}
         </ul>
       </div>
     )
