0f3ac3fa272ad7c95d4d112dd58787c3e5e94fb1
Celther <mwfister@gmail.com>
2018-07-28 22:59:17 +1000

    Loading initial data to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
 import Timeline from './Timeline'
+import handleInitialData from '../actions/shared'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +20,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default connect()(App)
