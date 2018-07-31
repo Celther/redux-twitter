503f9ac3e44aaef781ff7a33b066f08a2e5a0fcf
mwfister <mwfister@gmail.com>
2018-07-31 10:46:20 +1000

    Added Nav to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import Nav from './Nav'
 import Timeline from './Timeline'
 import ComposeTweet from './ComposeTweet'
 import ViewTweet from './ViewTweet'
@@ -15,17 +16,20 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="app-container">
-          {
-            this.props.loading ? null : (
-              <div>
-                <Route exact path="/" component={Timeline} />
-                <Route path="/new" component={ComposeTweet} />
-                <Route path="/view/:id" component={ViewTweet} />
-              </div>
-            )
-          }
-        </div>
+        <Fragment>
+          <Nav />
+          <div className="app-container">
+            {
+              this.props.loading ? null : (
+                <div>
+                  <Route exact path="/" component={Timeline} />
+                  <Route path="/new" component={ComposeTweet} />
+                  <Route path="/view/:id" component={ViewTweet} />
+                </div>
+              )
+            }
+          </div>
+        </Fragment>
       </Router>
     )
   }
