726c2ce171f980f2ee9b70457e16d81e00ec67ad
mwfister <mwfister@gmail.com>
2018-07-31 10:41:26 +1000

    [UI] Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
+
 
 export default function Nav(props) {
   return (
     <div className="nav">
-      NAV BAH
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClass="active">
+            Home
+          </NavLink>
+        </li>
+        <div>
+          <div className="nav-title">
+            <span><b>N</b>ot</span>
+            <span><b>Q</b>uite</span>
+            <span><b>T</b>witter</span>
+          </div>
+        </div>
+        <li>
+          <NavLink to="/new" activeClass="active">
+            New Tweet
+          </NavLink>
+        </li>
+      </ul>
     </div>
   )
 }
